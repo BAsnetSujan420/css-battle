c9dece98e551c0878b8a96fd83b0bd25a0ec8ed0
Sujan Basnet <basnetsujan1828420@gmail.com>
2026-03-25 17:46:45 +0545

    styled product cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -11,15 +11,70 @@ const GlobalStyles = createGlobalStyle`
     }
 `;
 
-const Container = styled.div``;
-const Wrapper = styled.div``;
-const ImageWrapper = styled.div``;
-const Image = styled.img``;
+const Container = styled.div`
+  background: var(--black);
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  padding: 50px;
+`;
+const Wrapper = styled.div`
+  max-width: 600px;
+  width: 100%;
+  color: var(--gray);
+  display: grid;
+  grid-template-columns: 100px auto 100px;
+  gap: 20px;
+  padding: 10px;
+  background: var(--charcoal);
+  border-radius: 5px;
+`;
+const ImageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const Image = styled.img`
+  width: 100px;
+  border-radius: 5px;
+  height: 80px;
+`;
 
-const Content = styled.div``;
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  color: var(--yellow);
+  font-weight: bold;
+  font-size: 15px;
 
-const ButtonContainer = styled.div``;
-const Button = styled.button``;
+  > div:nth-of-type(2) {
+    color: var(--gray);
+    font-weight: 400;
+    font-size: 12px;
+    max-width: 250px;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  justify-self: flex-end;
+`;
+const Button = styled.button`
+  width: 20px;
+  height: 20px;
+  color: var(--yellow);
+  background: var(--charcoal);
+  border-radius: 50%;
+  border: 1px solid var(--yellow);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function ProductCard() {
   return (
