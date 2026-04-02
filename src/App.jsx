7c9f3175da59a9cea20bb002f3d10b0e04278960
Sujan Basnet <basnetsujan1828420@gmail.com>
2026-04-02 10:53:51 +0545

    mad CSS : semi-finals

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import MR1 from './components/MR1';
 import ProductCard from './components/ProductCArd';
 import QF from './components/QF';
+import SM from './components/SM';
 
 function App() {
   return (
     <>
       {/* <MR1 /> */}
       {/* <ProductCard /> */}
-      <QF />
+      {/* <QF /> */}
+      <SM />
     </>
   );
 }
---
+++ src/components/SM.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+:root {
+--white: #ffffff;
+--orange: #ff7d1a;
+--light-orange: #ffede0;
+--gary: #68707d;
+--dark: #1d2025;
+--light-gray: #b6bcc8;
+}
+
+body {
+display: flex;
+width: 100%;
+height: 100svh;
+justify-content: center;
+align-items: center;
+background: var(--light-gray);
+}
+
+`;
+
+const Container = styled.div`
+  width: 600px;
+  height: 400px;
+`;
+const ProductDetails = styled.div``;
+const ProductThumbnails = styled.div``;
+const PurchaseControls = styled.div``;
+const ProductImage = styled.div``;
+
+const Image = styled.img``;
+const Thumbnail = styled.div``;
+const ThumbnailImage = styled.img``;
+
+const ProductPricing = styled.div``;
+const ProductBrand = styled.span``;
+const ProductTitle = styled.span``;
+const ProductDesc = styled.span``;
+const PriceRow = styled.div``;
+
+const CurrentPrice = styled.span``;
+const DiscountBadge = styled.span``;
+const OriginalPrice = styled.span``;
+const CartButton = styled.button``;
+
+function SM() {
+  return (
+    <>
+      <GlobalStyles />
+      <Container>
+        <ProductImage>
+          <Image
+            src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?auto=format&fit=crop&w=1200&q=80"
+            alt="air flex 9000s"
+          />
+        </ProductImage>
+        <ProductThumbnails>
+          <Thumbnail className="active">
+            <ThumbnailImage
+              src="https://images.unsplash.com/photo-1549298916-b41d501d3772?auto=format&fit=crop&w=300&q=60"
+              alt="air flex 9000s"
+            />
+          </Thumbnail>
+          <Thumbnail>
+            <ThumbnailImage
+              src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?auto=format&fit=crop&w=300&q=60"
+              alt="air flex 9000s"
+            />
+          </Thumbnail>
+          <Thumbnail>
+            <ThumbnailImage
+              src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=1200&q=80"
+              alt="air flex 9000s"
+            />
+          </Thumbnail>
+          <Thumbnail>
+            <ThumbnailImage
+              src="https://images.unsplash.com/photo-1608231387042-66d1773070a5?auto=format&fit=crop&w=1200&q=80"
+              alt="air flex 9000s"
+            />
+          </Thumbnail>
+        </ProductThumbnails>
+        <ProductDetails>
+          <ProductBrand>Hype Beast Inc.</ProductBrand>
+          <ProductTitle>Extremely Limited Edition Air Flex 9000s</ProductTitle>
+          <ProductDesc>
+            Finally Shoe you'll never actually wear. Kepp them in the box, post
+            them on Instagram, and feel superior to people who use footwear for
+            walking. Made with 100% artificial scarcity.
+          </ProductDesc>
+          <ProductPricing>
+            <PriceRow>
+              <CurrentPrice>$867.00</CurrentPrice>
+              <DiscountBadge>RESALE</DiscountBadge>
+            </PriceRow>
+            <OriginalPrice>Retail was $210</OriginalPrice>
+          </ProductPricing>
+        </ProductDetails>
+        <PurchaseControls>
+          <CartButton>Add to Cart</CartButton>
+        </PurchaseControls>
+      </Container>
+    </>
+  );
+}
+
+export default SM;
